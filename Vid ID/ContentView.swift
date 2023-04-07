efb781a615ab8b0eae1b8b34a7f37f00da789336
fajaragungpramana01 <fajar.agungpramana@mareco.id>
2023-04-07 10:27:56 +0700

    initial commit

---
+++ Vid ID/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Vid ID
+//
+//  Created by Fajar Agung Pramana on 07/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vid ID/Vid_IDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vid_IDApp.swift
+//  Vid ID
+//
+//  Created by Fajar Agung Pramana on 07/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Vid_IDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
