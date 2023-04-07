390f896db4809d5d6a894c5318778605e2eec6d8
fajaragungpramana <fajar.agungpramana01@gmail.com>
2023-04-07 16:46:42 +0700

    create main view and navigation it.

---
+++ Vid ID/Ui/Main/MainView.swift
@@ -0,0 +1,25 @@
+//
+//  MainView.swift
+//  Vid ID
+//
+//  Created by Fajar Agung Pramana on 07/04/23.
+//
+
+import SwiftUI
+
+struct MainView : View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+        .navigationBarBackButtonHidden(true)
+        
+    }
+}
+
+struct MainView_Previews : PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
--- Vid ID/Ui/Splash/SplashView.swift
+++ Vid ID/Ui/Splash/SplashView.swift
@@ -8,15 +8,37 @@
 import SwiftUI
 
 struct SplashView : View {
+    
+    // MARK: State
+    @State private var mIsLoadingDone = false
+    
     var body: some View {
         
-        VStack {
-            Text("app_name")
-                .foregroundColor(ColorResource.APP_PRIMARY)
-                .font(.custom(FontResource.POPPINS_SEMI_BOLD, size: DimenResource.SIZE_32))
+        NavigationStack {
+            
+            VStack {
+                Text("app_name")
+                    .foregroundColor(ColorResource.APP_PRIMARY)
+                    .font(.custom(FontResource.POPPINS_SEMI_BOLD, size: DimenResource.SIZE_32))
+            }
+            .navigationDestination(isPresented: $mIsLoadingDone) {
+                MainView()
+            }
+            
+        }.onAppear {
+            self.gotoMainView()
         }
         
     }
+    
+    private func gotoMainView() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            withAnimation {
+                self.mIsLoadingDone = true
+            }
+        }
+    }
+    
 }
 
 struct SplashView_Previews : PreviewProvider {
