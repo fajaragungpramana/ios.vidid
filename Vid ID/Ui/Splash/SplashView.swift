b08cc8c48d7135deffd740f201e21ea10e16fa80
fajaragungpramana <fajar.agungpramana01@gmail.com>
2023-04-07 10:52:07 +0700

    Add splash view

---
+++ Vid ID/Ui/Splash/SplashView.swift
@@ -0,0 +1,24 @@
+//
+//  SplashView.swift
+//  Vid ID
+//
+//  Created by Fajar Agung Pramana on 07/04/23.
+//
+
+import SwiftUI
+
+struct SplashView : View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+        
+    }
+}
+
+struct SplashView_Previews : PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
