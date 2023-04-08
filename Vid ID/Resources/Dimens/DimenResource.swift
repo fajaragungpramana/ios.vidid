a13ba09fe6eadc7cc483acd5ed9167f498e7417b
fajaragungpramana <fajar.agungpramana01@gmail.com>
2023-04-08 14:49:48 +0700

    Create title and search field on main view.

--- Vid ID/Resources/Dimens/DimenResource.swift
+++ Vid ID/Resources/Dimens/DimenResource.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 struct DimenResource {
+    static let SIZE_8: CGFloat = 8
+    static let SIZE_10: CGFloat = 10
+    static let SIZE_12: CGFloat = 12
+    static let SIZE_14: CGFloat = 14
+    static let SIZE_16: CGFloat = 16
+    static let SIZE_24: CGFloat = 24
     static let SIZE_32: CGFloat = 32
+    static let SIZE_40: CGFloat = 40
 }
--- Vid ID/Ui/Main/MainView.swift
+++ Vid ID/Ui/Main/MainView.swift
@@ -12,20 +12,40 @@ struct MainView : View {
     
     @ObservedObject private var mViewModel = MainViewModel()
     
+    @State private var mSearchQuery: String = ""
+    
     var body: some View {
         
         VStack {
+            
+            // MARK: App Toolbar Title
+            Text("app_name")
+                .foregroundColor(ColorResource.TEXT_PRIMARY)
+                .font(.custom(FontResource.POPPINS_SEMI_BOLD, size: DimenResource.SIZE_24))
+            
+            // MARK: Search Video
+            HStack {
+                
+                Image(DrawableResource.IC_SEARCH)
+                    .padding([.vertical, .trailing], DimenResource.SIZE_8)
+                    .padding([.leading], DimenResource.SIZE_12)
+                
+                TextField("find_your_favorite_here", text: $mSearchQuery)
+                    .foregroundColor(ColorResource.TEXT_PRIMARY)
+                    .font(.custom(FontResource.POPPINS_REGULAR, size: DimenResource.SIZE_14))
+                
+            }
+            .background(RoundedRectangle(
+                cornerRadius: DimenResource.SIZE_10
+            ).fill(ColorResource.DIVIDER_PRIMARY))
+            .frame(height: DimenResource.SIZE_40, alignment: .center)
+            .padding([.horizontal], DimenResource.SIZE_16)
             
         }
         .navigationBarBackButtonHidden(true)
         .onAppear {
             NetworkActivityLogger.shared.level = .debug
             NetworkActivityLogger.shared.startLogging()
-            
-            self.mViewModel.getListTrending(request: TrendingRequest(
-                mediaType: .ALL,
-                timeWindow: .WEEK)
-            )
         }
         
     }
