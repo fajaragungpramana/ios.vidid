8042d0a2f2aeefdf8c6b9f9c5227e3c06c16f538
fajaragungpramana <fajar.agungpramana01@gmail.com>
2023-04-11 08:19:49 +0700

    Add back btn.

--- Vid ID/Ui/Video/VideoView.swift
+++ Vid ID/Ui/Video/VideoView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 
 struct VideoView : View {
     
+    // MARK: Environment
+    @Environment(\.presentationMode) private var mMode: Binding<PresentationMode>
+    
     var body: some View {
         VStack {
             
+            HStack {
+                
+                Button {
+                    self.mMode.wrappedValue.dismiss()
+                } label: {
+                    Image(DrawableResource.IC_BACK_BLACK)
+                        .renderingMode(.original)
+                }
+                .padding(.horizontal, DimenResource.SIZE_16)
+                
+                Spacer()
+                
+            }
             
+            ScrollView {
+                
+            }
             
         }
         .navigationBarBackButtonHidden(true)
